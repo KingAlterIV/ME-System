b79a1e4c77cb55648e814b2c71189e29d18cb506
Parker Hawke <hawkeboyz2@hotmail.com>
2020-08-02 17:07:30 -0400

    Merge and simplify block listeners

--- src/main/java/tk/kingalteriv/mesystem/MESystem.java
+++ src/main/java/tk/kingalteriv/mesystem/MESystem.java
@@ -1,13 +1,14 @@
 package tk.kingalteriv.mesystem;
 
 import co.aikar.commands.PaperCommandManager;
+
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import tk.kingalteriv.mesystem.block.MEStateHandler;
 import tk.kingalteriv.mesystem.commands.MESystemCommand;
-import tk.kingalteriv.mesystem.listeners.METerminalPlacementListener;
-import tk.kingalteriv.mesystem.listeners.MEDrivePlacementListener;
+import tk.kingalteriv.mesystem.listeners.MEBlockPlacementListener;
 import tk.kingalteriv.mesystem.utilities.ReflectionUtils;
 import tk.kingalteriv.mesystem.utilities.loader.ItemLoader;
 
@@ -15,7 +16,7 @@ public class MESystem extends JavaPlugin {
 
     public static ItemStack ITEM_ME_SYSTEM;
     public static ItemStack ITEM_ME_DRIVE;
-    
+
     private static MESystem instance;
 
     private PaperCommandManager commandManager;
@@ -35,8 +36,7 @@ public class MESystem extends JavaPlugin {
 
         ItemLoader.loadAllItems();
 
-        Bukkit.getPluginManager().registerEvents(new METerminalPlacementListener(this), this);
-        Bukkit.getPluginManager().registerEvents(new MEDrivePlacementListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new MEBlockPlacementListener(this), this);
     }
 
     public PaperCommandManager getManager() {
---
+++ src/main/java/tk/kingalteriv/mesystem/listeners/MEBlockPlacementListener.java
@@ -0,0 +1,82 @@
+package tk.kingalteriv.mesystem.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Dispenser;
+import org.bukkit.block.Dropper;
+import org.bukkit.block.data.Directional;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import tk.kingalteriv.mesystem.MEItem;
+import tk.kingalteriv.mesystem.MESystem;
+import tk.kingalteriv.mesystem.block.BlockMEDrive;
+import tk.kingalteriv.mesystem.block.BlockMETerminal;
+
+public class MEBlockPlacementListener implements Listener {
+
+    private final MESystem plugin;
+
+    public MEBlockPlacementListener(MESystem plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void blockPlacementListener(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        if (action != Action.RIGHT_CLICK_BLOCK && action != Action.RIGHT_CLICK_AIR) {
+            return;
+        }
+
+        Block clickedBlock = event.getClickedBlock().getRelative(event.getBlockFace());
+        if (clickedBlock == null || clickedBlock.getType() != Material.AIR) {
+            return;
+        }
+
+        ItemStack item = event.getItem();
+        if (!MESystem.ITEM_ME_SYSTEM.isSimilar(item)) {
+            event.setCancelled(true);
+
+            if (action == Action.RIGHT_CLICK_BLOCK) {
+                clickedBlock.setType(Material.DISPENSER);
+                BlockState state = clickedBlock.getState();
+                if (state instanceof Dispenser) {
+                    MEItem meItem = new MEItem(item);
+
+                    Directional blockData = (Directional) clickedBlock.getBlockData();
+                    blockData.setFacing(event.getPlayer().getFacing().getOppositeFace());
+                    clickedBlock.setBlockData(blockData);
+
+                    BlockMETerminal core = new BlockMETerminal(clickedBlock, false);
+                    core.setTerminal(true);
+                    core.setSlotAmount(meItem.getSlotAmount());
+                    core.writeToState();
+
+                    this.plugin.getStateHandler().registerCore(core);
+                }
+            }
+        }
+        else if (!MESystem.ITEM_ME_DRIVE.isSimilar(item)) {
+            event.setCancelled(true);
+
+            if (action == Action.RIGHT_CLICK_BLOCK) {
+                clickedBlock.setType(Material.DROPPER);
+                BlockState state = clickedBlock.getState();
+                if (state instanceof Dropper) {
+                    Directional blockData = (Directional) clickedBlock.getBlockData();
+                    blockData.setFacing(event.getPlayer().getFacing().getOppositeFace());
+                    clickedBlock.setBlockData(blockData);
+
+                    BlockMEDrive drive = new BlockMEDrive(clickedBlock, false);
+                    drive.setDrive(true);
+                    drive.writeToState();
+                }
+            }
+        }
+    }
+
+}
